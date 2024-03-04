c811ac59341e7cdfa3c3992f9ed0eda293ccf00f
Michael <michaelmwendwamumo@gmail.com>
2024-03-04 22:30:52 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, tester_list):
+        self.tester_list = tester_list
+
+        sorted_word_letters = sorted([letter for letter in self.word])
+        matched_word = []
+    
+        for tester in tester_list:
+            sorted_tester_letters = sorted([letter for letter in tester])
+            if sorted_word_letters == sorted_tester_letters:
+                matched_word.append(tester)
+        return matched_word
+    
+Anagram("enlist").match(["listen", "poison", "hello"])
+
+        
+
+
